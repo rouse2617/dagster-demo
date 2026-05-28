75c2c5227c8bc3d2180073720b92e01642c5b69f
ruipeng.huang <ruipeng.huang@cyberorigin.ai>
2026-05-28 11:13:18 +0800

    Simplify definitions.py, use load_assets_from_dbt_project, add dbt-duckdb dep

--- dagster_demo/definitions.py
+++ dagster_demo/definitions.py
@@ -1,17 +1,6 @@
 from dagster import Definitions
-from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
+from dagster_dbt import load_assets_from_dbt_project
 
-dagster_demo_project = DbtProject(project_dir=".")
-dagster_demo_project.prepare_if_dev()
+dbt_assets = load_assets_from_dbt_project(project_dir=".")
 
-
-@dbt_assets(manifest=dagster_demo_project.manifest_path)
-def dagster_demo_assets(context):
-    yield from (
-        DbtCliResource(project_dir=".")
-        .cli(["build"], context=context)
-        .stream()
-    )
-
-
-defs = Definitions(assets=[dagster_demo_assets])
+defs = Definitions(assets=dbt_assets)
